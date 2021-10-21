1bf03bb650bfd404a24779ee1179335e9923894f
igorxcardoso <iygorsilva@gmail.com>
2021-10-21 13:53:27 -0300

    Criação do create artigo. Log de exec.

--- devnews/app/controllers/artigos_controller.rb
+++ devnews/app/controllers/artigos_controller.rb
@@ -9,4 +9,28 @@ class ArtigosController < ApplicationController
     # O para params[:id] vai trazer o id do artigo
     # O Rails pega o parâmetro da URL com params[:id]
   end
+
+	def new
+		@artigo = Artigo.new()
+	end
+
+	def create
+		@artigo = Artigo.new(params)
+
+		if @artigo.save()
+			redirect_to(@artigo)
+			# Quando redireciono para @artigo, então o Rails vai redirecionar para o show
+		else
+			render(:new)
+			# Vai enderizar apenas a view de new
+			# Se desse um redirect_to() perdeiria o que já havia sido digitado
+		end
+	end
+
+
+	private
+	
+	def artigo_params
+		params
+	end
 end
